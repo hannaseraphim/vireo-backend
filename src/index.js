ca1056fcde11c9c1cdfba4028ee72195e683dbcc
Hanna Seraphim <hannaseraphim@gmail.com>
2025-09-22 13:49:24 -0400

    Updates the CORS to Vercel App

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173'}))
+app.use(cors({origin: 'https://vireo-kappa.vercel.app'}))
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
